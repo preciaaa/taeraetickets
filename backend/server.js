ef6724dbb3503b6103707aaac69f2ddf8228e363
juallybeans <jenis.onn.2023@scis.smu.edu.sg>
2025-07-05 20:55:47 +0800

    cart backend done

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const authService = require('./services/authService');
 const userService = require('./services/userService');
 const recaptchaService = require('./services/recaptchaService');
 const facialRecognition = require('./services/facialRecognition');
+const cartService = require('./services/cartService');  
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use('/', authService);
 app.use('/', userService);
 app.use('/', recaptchaService);
 app.use('/', facialRecognition);
+app.use('/', cartService);
 
 // Start the server
 const port = process.env.PORT || 3000;
