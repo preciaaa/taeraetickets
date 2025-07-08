41ee27a6f6f662ac4e65dd5199e7edd862e084d0
kirthivasshni <kirthiv.l.2023@scis.smu.edu.sg>
2025-07-08 19:26:06 +0800

    fixed a smol merge error

--- frontend/app/my-listings/page.tsx
+++ frontend/app/my-listings/page.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
-import { supabase } from '@/lib/supabaseClient'
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
